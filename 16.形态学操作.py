cbfd1ed5ef73e75759f33754ee1c5a2367f5dec2
bigleftgo <47958618+bigleftgo@users.noreply.github.com>
2019-06-20 09:48:38 +0800

    Update 16.形态学操作.py

--- 16.形态学操作.py
+++ 16.形态学操作.py
@@ -69,12 +69,12 @@ def tophat_morphology_demo(image): #顶帽操作
     cv.imshow("top_hat_demo", dst)
 
 
-def blackHat_morphology_demo(image): #顶帽操作
+def blackHat_morphology_demo(image): #黑帽操作
     gray=cv.cvtColor(image,cv.COLOR_BGR2GRAY)
     kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
     dst = cv.morphologyEx(gray, cv.MORPH_BLACKHAT, kernel=kernel)
-    cimg = np.array(gray.shape, np.uint8)
-    cimg = 0
+    cimg = np.array(gray.shape, np.uint8)#构造数组
+    cimg = 0#增加亮度
     dst = cv.add(dst, cimg)
     cv.imshow("BLACK_hat_demo", dst)
 
